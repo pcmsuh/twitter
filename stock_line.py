e9250b19addacd1375c05424e81f791d89bbff81
Philip Suh <120293593+pcmsuh@users.noreply.github.com>
2023-05-18 21:04:08 +0900

    edit stock_line, update file names

--- stock_line.py
+++ stock_line.py
@@ -9,6 +9,9 @@ plt.rcParams['font.size'] = 10
 
 
 def graph_data(stock):
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((1, 1), (0, 0))
+
     stock_price_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={stock}&apikey={key}'
     
     response = requests.get(stock_price_url)
@@ -20,11 +23,16 @@ def graph_data(stock):
     df = df.astype(float)
     df.index = pd.to_datetime(df.index)
     
-    plt.plot(df.index, df['4. close'])
-    
+    ax1.plot(df.index, df['4. close'], label='Price')
+    for label in ax1.get_xticklabels():
+        label.set_rotation(45)
+    ax1.grid(True, color = 'grey', linestyle='-', linewidth=0.25)
+
     plt.xlabel('Date')
     plt.ylabel('Close Price')
     plt.title('Daily Adjusted Close Prices')
+    plt.legend()
+    plt.subplots_adjust(left=0.15, bottom=0.25, right=0.9, top=0.85, wspace=0.2, hspace=0)
     plt.show()
 
 
